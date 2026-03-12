d24c3e40aaa4b25be60354b977c8e0456be6939a
Vhea He <98002993+vhea-h@users.noreply.github.com>
2026-03-12 18:47:33 -0400

    Added q1.sql solution from warmup
    This contains my solution for Q1: Unrated Products from the A2 warmup

--- Assignment_2/eshal_and_vhea_solutions/part1/q1.sql
+++ Assignment_2/eshal_and_vhea_solutions/part1/q1.sql
@@ -1,5 +1,5 @@
 -- Unrated products
-
+-- Vhea's Solution
 
 -- You must not change the next 2 lines or the table definition.
 SET SEARCH_PATH TO Recommender;
@@ -12,14 +12,47 @@ CREATE TABLE q1(
     email TEXT	
 );
 
--- You may find it convenient to do this for each of the views
--- that define your intermediate steps. (But give them better names!)
-DROP VIEW IF EXISTS IntermediateStep CASCADE;
+-- List of Unreveiwed Items
+DROP VIEW IF EXISTS UnreviewedItems CASCADE;
 
--- Define views for your intermediate steps here:
-CREATE VIEW IntermediateStep AS ... ;
+CREATE VIEW UnreviewedItems AS 
+    SELECT DISTINCT item.IID AS IID
+    FROM item
+    EXCEPT
+    SELECT DISTINCT review.IID AS IID
+    FROM review;
+
+-- List of unreviewed items that have been bought
+DROP VIEW IF EXISTS Unreviewed_Purchases CASCADE;
+
+CREATE VIEW Unreviewed_Purchases AS 
+    SELECT lineitem.pid AS U_PID, lineitem.iid AS U_IID
+    FROM lineitem INNER JOIN UnreviewedItems ON lineitem.IID = UnreviewedItems.IID;
 
 
+DROP VIEW IF EXISTS Unreviewed_Purchase_Info CASCADE;
+
+CREATE VIEW Unreviewed_Purchase_Info AS 
+    SELECT *
+    FROM purchase INNER JOIN Unreviewed_Purchases ON  purchase.pid = Unreviewed_Purchases.U_PID;
+
+-- List of customers and the amount of distinct unreviewed things they've bought if they've bought more than 3
+DROP VIEW IF EXISTS People_Bought CASCADE;
+
+CREATE VIEW People_Bought AS 
+    SELECT Unreviewed_Purchase_Info.cid AS customer_id, COUNT(DISTINCT Unreviewed_Purchase_Info.U_IID) AS diff_things_bought
+    FROM Unreviewed_Purchase_Info
+    GROUP BY Unreviewed_Purchase_Info.cid
+    HAVING COUNT(DISTINCT Unreviewed_Purchase_Info.U_IID) >= 3;
+
+-- List of customers who bought unreviewed items 
+DROP VIEW IF EXISTS Unrev_Customer_Info CASCADE;
+
+CREATE VIEW Unrev_Customer_Info AS 
+    SELECT DISTINCT *
+    FROM customer INNER JOIN People_Bought ON customer.cid = People_Bought.customer_id;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
+SELECT cid, first_name, last_name, email
+FROM Unrev_Customer_Info;
