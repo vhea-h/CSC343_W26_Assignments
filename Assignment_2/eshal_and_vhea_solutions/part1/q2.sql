5aaeec3f052cc2f64e2621eaefca68750808cb0b
4eshalahmed <4eshalahmed@gmail.com>
2026-03-14 13:11:56 -0400

    Update q2.sql

--- Assignment_2/eshal_and_vhea_solutions/part1/q2.sql
+++ Assignment_2/eshal_and_vhea_solutions/part1/q2.sql
@@ -19,12 +19,12 @@ DROP VIEW IF EXISTS ReviewCounts CASCADE;
 -- Step 1: Find reviews that are considered "helpful"
 -- A review is helpful if TRUE votes > FALSE votes
 CREATE VIEW HelpfulReviews AS
-    SELECT reviewer AS cid, iid
-    FROM Helpfulness
-    GROUP BY reviewer, iid
-    HAVING
-        SUM(CASE WHEN helpfulness = TRUE THEN 1 ELSE 0 END) >
-        SUM(CASE WHEN helpfulness = FALSE THEN 1 ELSE 0 END);
+SELECT reviewer AS cid, iid
+FROM Helpfulness
+GROUP BY reviewer, iid
+HAVING
+    SUM(CASE WHEN helpfulness = TRUE THEN 1 ELSE 0 END) >
+    SUM(CASE WHEN helpfulness = FALSE THEN 1 ELSE 0 END);
 
 
 -- Step 2: Count total reviews and helpful reviews for each customer
@@ -40,24 +40,23 @@ CREATE VIEW ReviewCounts AS
 
 -- Step 3: Each cutomers CID, their name, and their helpfulness score
 INSERT INTO q2
-SELECT
-    c.cid AS cid,
-    c.first_name || ' ' || c.last_name AS name,
+    SELECT
+        c.cid AS cid,
+        c.first_name || ' ' || c.last_name AS name,
 
-    CASE
-        -- If a customer has no reviews then score = 0
-        WHEN rc.total_reviews = 0 
-            THEN 'not helpful'
+        CASE
+            -- If a customer has no reviews then score = 0
+            WHEN rc.total_reviews = 0 THEN 'not helpful'
 
-        -- Otherwise, score = helpful_reviews / total_reviews
-        
-        WHEN (rc.helpful_reviews / rc.total_reviews) >= 0.8
-            THEN 'very helpful'
+            -- Otherwise, score = helpful_reviews / total_reviews
+            
+            WHEN (rc.helpful_reviews::FLOAT / rc.total_reviews) >= 0.8
+                THEN 'very helpful'
 
-        WHEN (rc.helpful_reviews / rc.total_reviews) >= 0.5
-            THEN 'somewhat helpful'
+            WHEN (rc.helpful_reviews::FLOAT / rc.total_reviews) >= 0.5
+                THEN 'somewhat helpful'
 
-        ELSE 'not helpful'
-    END AS helpfulness_category
+            ELSE 'not helpful'
+        END AS helpfulness_category
 
-FROM Customer c LEFT JOIN ReviewCounts rc ON c.cid = rc.cid;
+    FROM Customer c LEFT JOIN ReviewCounts rc ON c.cid = rc.cid;
