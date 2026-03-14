770a231a3131019446ce7609201088128de411d4
Vhea He <98002993+vhea-h@users.noreply.github.com>
2026-03-14 17:31:34 -0400

    Added solution for q5.sql

--- Assignment_2/eshal_and_vhea_solutions/part1/q5.sql
+++ Assignment_2/eshal_and_vhea_solutions/part1/q5.sql
@@ -1,4 +1,5 @@
 -- Hyperconsumers
+-- Vhea's Solution
 
 -- You must not change the next 2 lines or the table definition.
 SET SEARCH_PATH TO Recommender;
@@ -11,14 +12,55 @@ CREATE TABLE q5 (
     items INTEGER NOT NULL
 );
 
--- You may find it convenient to do this for each of the views
--- that define your intermediate steps. (But give them better names!)
-DROP VIEW IF EXISTS IntermediateStep CASCADE;
+-- The customer, the amount of items they bought, and when they bought it
+-- Columns: CID, quantity, checkout-time 
+DROP VIEW IF EXISTS AllPurchases CASCADE;
+CREATE VIEW AllPurchases AS
+    SELECT Purchase.CID, LineItem.quantity, Purchase.checkout_time
+    FROM Purchase NATURAL JOIN LineItem;
 
--- Define views for your intermediate steps here:
-CREATE VIEW IntermediateStep AS ... ;
+-- A list containing the year, the customer and the total amount of items they bought 
+-- Columns: year, CID, total_purchases
+DROP VIEW IF EXISTS AnnualPurchases CASCADE;
+CREATE VIEW AnnualPurchases AS
+    SELECT EXTRACT (Year FROM AllPurchases.checkout_time) AS year, AllPurchases.CID, sum(AllPurchases.quantity) AS total_purchases
+    FROM AllPurchases
+    GROUP BY year, CID;
 
+-- Gets the top 5 disctinct quantities of the year, will only be less if there are less than 5 values in the year 
+-- Columns: year, total_quantity 
+DROP VIEW IF EXISTS OrderedQuantities CASCADE;
+CREATE VIEW OrderedQuantities AS
+    SELECT DISTINCT year, total_purchases, 
+        DENSE_RANK() OVER (PARTITION BY year ORDER BY total_purchases DESC) AS rank
+    FROM AnnualPurchases 
+    ORDER BY year, total_purchases;
 
+DROP VIEW IF EXISTS Top5Quantities CASCADE;
+CREATE VIEW Top5Quantities AS
+    SELECT DISTINCT year, total_purchases
+    FROM OrderedQuantities
+    WHERE rank <= 5
+    ORDER BY year, total_purchases DESC;
+
+DROP VIEW IF EXISTS Top5Customers CASCADE;
+CREATE VIEW Top5Customers AS
+    SELECT * 
+    FROM AnnualPurchases 
+    WHERE EXISTS (
+        SELECT * 
+        FROM Top5Quantities 
+        WHERE AnnualPurchases.year = Top5Quantities.year 
+            AND AnnualPurchases.total_purchases = Top5Quantities.total_purchases
+    )
+    ORDER BY year, total_purchases DESC;
+
+DROP VIEW IF EXISTS Hyperconsumers CASCADE;
+CREATE VIEW Hyperconsumers AS
+    SELECT Top5Customers.year, CONCAT(Customer.first_name, ' ', Customer.last_name) AS name, Customer.email, Top5Customers.total_purchases AS items 
+    FROM Top5Customers NATURAL JOIN Customer;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q5
+    SELECT year, name, email, items 
+    FROM Hyperconsumers;
