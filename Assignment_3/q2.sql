93a6d0746834534bfcc3512fd7993c30d6274b7e
Vhea He <98002993+vhea-h@users.noreply.github.com>
2026-03-23 22:19:04 -0400

    Added solution to q2.sql

--- Assignment_3/q2.sql
+++ Assignment_3/q2.sql
@@ -1,1 +1,11 @@
+SET SEARCH_PATH TO TicketSchema;
 
+-- An owner and how many venues they own
+-- Columns: ownerID, num_venues_owned
+DROP VIEW IF EXISTS OwnedVenueCount CASCADE;
+CREATE VIEW OwnedVenueCount AS
+    SELECT Owner.ownerID, count(Venue.venueID) as num_venues_owned
+    FROM Owner LEFT JOIN Venue 
+        ON Owner.ownerID = Venue.ownerID
+    GROUP BY Owner.ownerID
+    ORDER BY count(Venue.venueID) DESC, Owner.ownerID;
